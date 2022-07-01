b98c89ff4c01329c09e57b94e1a30b39c146e21d
Axel <axellesca@gmail.com>
2022-07-01 20:11:50 -0300

    fix errors

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" /*crossOrigin*//>
                 <link href="https://fonts.googleapis.com/css2?family=Sen&display=swap" rel="stylesheet"/>
             </Head>
             <body>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Footer } from '../components/Footer'
 import { Navbar } from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
@@ -9,13 +10,20 @@ export default function Home() {
       <Head>
         <title>Portfolio Next</title>
         <meta name="description" content="Portfolio Personal para muestra de Proyectos" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta accessKey='portfolio, linkedin, github' />
         <link rel="icon" href="/favicon.ico" />
         {/* <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Sen&display=swap" rel="stylesheet"/> */}
       </Head>
-
+      {/* NavBar */}
       <Navbar/>
+
+
+
+      {/* Footer */}
+      <Footer/>
     </div>
   )
 }
