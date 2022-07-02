b6d63085896791c8f618fc67948be72aef2f51d6
Axel <axellesca@gmail.com>
2022-07-01 21:22:18 -0300

    create footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,34 +7,35 @@ import { Box, Icon, Stack } from '@chakra-ui/react';
 
 export const Footer = () => {
     return (
-        <Stack display={"flex"} flexDirection={"row"} flexWrap={"wrap"} justifyContent={"space-evenly"} alignItems={"flex-end"}>
-            {/* <br></br> */}
-            {/*Hacer Button para descargar CV*/}
-            {/* <Box>
-                <Icon w={50} h={50}>
-                    <MdContactPage/>
-                </Icon>
-            </Box> */}
-            <Box /* w={"50px"} h={"50px"} */>
-                <Icon w={50} h={50}>
-                    <AiFillMail/>
-                </Icon>
-            </Box>
-            <Box /* w={"50px"} h={"50px"} */>
-                <a href="https://linkedin.com/in/axel-lescano" target="_blank" rel="noopener noreferrer">
+        <Box>
+            <Stack display={"flex"} flexDirection={"row"} flexWrap={"wrap"} justifyContent={"space-evenly"} alignItems={"flex-end"}>
+                {/* <br></br> */}
+                {/*Hacer Button para descargar CV*/}
+                {/* <Box>
                     <Icon w={50} h={50}>
-                        <AiFillLinkedin/>
+                        <MdContactPage/>
                     </Icon>
-                </a>
-            </Box>
-            <Box /* w={"50px"} h={"50px"} */>
-                <a href="https://github.com/Axellesca" target="_blank" rel="noopener noreferrer">
+                </Box> */}
+                <Box /* w={"50px"} h={"50px"} */>
                     <Icon w={50} h={50}>
-                        <AiOutlineGithub/>
+                        <AiFillMail/>
                     </Icon>
-                </a>
-            </Box>
-        </Stack>
-        
+                </Box>
+                <Box /* w={"50px"} h={"50px"} */>
+                    <a href="https://linkedin.com/in/axel-lescano" target="_blank" rel="noopener noreferrer">
+                        <Icon w={50} h={50}>
+                            <AiFillLinkedin/>
+                        </Icon>
+                    </a>
+                </Box>
+                <Box /* w={"50px"} h={"50px"} */>
+                    <a href="https://github.com/Axellesca" target="_blank" rel="noopener noreferrer">
+                        <Icon w={50} h={50}>
+                            <AiOutlineGithub/>
+                        </Icon>
+                    </a>
+                </Box>
+            </Stack>
+        </Box>
     )
 }
